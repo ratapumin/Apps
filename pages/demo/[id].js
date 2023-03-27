d2f335139f5c68b1668b28453652b43ba5c05008
Ratapumin Naudom <ratapumin@gmail.com>
2023-03-27 14:12:02 +0700

    can update title , desrciption

--- pages/demo/[id].js
+++ pages/demo/[id].js
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 
 export default function CallID() {
   const [data, setData] = useState({});
+
   const [jwt, setJwt] = useState("");
   const router = useRouter();
   const [updatedData, setUpdatedData] = useState("");
@@ -41,6 +42,12 @@ export default function CallID() {
     return <div>Loading...</div>;
   }
 
+  const newUpdatedData = {
+    Pic: data.attributes.Pic.data.attributes.url,
+    Title: data.attributes.Title,
+    Descriptions: data.attributes.Descriptions,
+  };
+
   return (
     <>
       <center>
@@ -55,12 +62,22 @@ export default function CallID() {
             <p>Descriptions : {data.attributes.Descriptions}</p>
             <input
               type="file"
+              file=""
               onChange={(e) =>
                 setData({
                   ...data,
                   attributes: {
                     ...data.attributes,
-                    Pic: e.target.files[0],
+                    Pic: {
+                      ...data.attributes.Pic,
+                      data: {
+                        ...data.attributes.Pic.data,
+                        attributes: {
+                          ...data.attributes.Pic.data.attributes,
+                          url: URL.createObjectURL(e.target.files[0]),
+                        },
+                      },
+                    },
                   },
                 })
               }
@@ -94,19 +111,15 @@ export default function CallID() {
             <br />
             <button
               onClick={() => {
-                const newUpdatedData = {
-                  ...updatedData,
-                  Title: data.attributes.Title,
-                  Descriptions: data.attributes.Descriptions,
-                  Pic: data.attributes.Pic.attributes,
-                };
                 setUpdatedData(newUpdatedData);
+                console.log(newUpdatedData);
                 const selectedItemId = localStorage.getItem("selectedItemId");
                 axios
                   .put(
                     `http://localhost:1337/api/apps/${selectedItemId}?populate=*`,
                     {
                       data: {
+                        Pic: data.attributes.Pic.data.attributes.url,
                         Title: data.attributes.Title,
                         Descriptions: data.attributes.Descriptions,
                       },
@@ -118,16 +131,19 @@ export default function CallID() {
                     }
                   )
                   .then((res) => {
+                    console.log("Record updated successfully:", res.data);
                     window.alert("Record updated successfully:", res.data);
                     setIsEditing(false);
                   })
                   .catch((error) => {
+                    console.error("Error updating record:", error);
                     window.alert("Error updating record:", error);
                   });
               }}
             >
               Save
             </button>
+
             <button onClick={() => setIsEditing(false)}>Cancel</button>
           </div>
         ) : (
